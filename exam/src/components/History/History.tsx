f2dea90e51ac7b24e5658b981813ed2bb72f4089
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2025-01-08 00:41:41 +0500

    fix

--- exam/src/components/History/History.tsx
+++ exam/src/components/History/History.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {RootState} from "../../redux/store";
-import {clearHistory} from "../../redux/translationSlice";
-import {ArrowBack} from "@mui/icons-material";
-import {useNavigate} from "react-router-dom";
-import './historyStyles.css'
+import { RootState } from "../../redux/store";
+import { loadTranslations, clearHistory } from "../../redux/translationSlice";
+import { ArrowBack } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import './historyStyles.css';
 
 const History: React.FC = () => {
     const translations = useSelector((state: RootState) => state.translation.translations);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        dispatch(loadTranslations());
+    }, [dispatch]);
+
     return (
         <div>
             <div className="header">
                 <h1>История</h1>
-                <ArrowBack className="arrow-back" onClick={()=>navigate('/')}/>
+                <ArrowBack className="arrow-back" onClick={() => navigate('/')} />
             </div>
             <button className="clear-button" onClick={() => dispatch(clearHistory())}>Очистить историю</button>
             {translations.length === 0 ? (
@@ -23,21 +27,19 @@ const History: React.FC = () => {
             ) : (
                 <ul>
                     {translations.map((item, index) => (
-                        <>
-                            <hr/>
-                            <li key={index}>
+                        <React.Fragment key={index}>
+                            <hr />
+                            <li>
                                 <p>{item.sourceLang} → {item.targetLang}</p>
                                 <p className="original-text">{item.originalText}</p>
                                 <p className="translated-text">{item.translatedText}</p>
                             </li>
-                        </>
-            ))}
-        </ul>
-    )
-}
-</div>
-)
-    ;
+                        </React.Fragment>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 };
 
 export default History;
